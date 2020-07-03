2c5c8eccb7056e346c13620c81b9c92fcf9323c6
JP-2000 <jitenpatel1482000@gmail.com>
2020-07-03 12:46:53 +0530

    Used video player pludgin in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_projects/maps/quakes.dart';
 import 'package:flutter_projects/maps/simple_google_maps.dart';
 import 'package:flutter_projects/parsing_json/json_parsing_maps.dart';
 import 'package:flutter_projects/weather_forecast/weather_forecast.dart';
+import 'myapp/simple_video_player.dart';
 import 'ui/home.dart';
 import 'ui/mortgage_app.dart';
 import 'package:flutter_projects/parsing_json/json_parsing.dart';
@@ -31,7 +32,6 @@ TextTheme appTextTheme(TextTheme base) {
   );
 }
 
-
 void main() => runApp(
   new MaterialApp(
     theme: ThemeData.dark(),
@@ -41,3 +41,93 @@ void main() => runApp(
 );
 //void main() => runApp(Home());
 
+// Video player plugin
+/*
+//Video_player
+
+//import 'package:flutter/material.dart';
+//import 'package:video_player/video_player.dart';
+//
+//void main() {
+//  runApp(Home());
+//}
+//
+//class Home extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp(
+//      title: 'Video Example',
+//      home: VideoExample(),
+//    );
+//  }
+//}
+//
+//class VideoExample extends StatefulWidget {
+//  @override
+//  VideoState createState() => VideoState();
+//}
+//
+//class VideoState extends State<VideoExample> {
+//  VideoPlayerController playerController;
+//  VoidCallback listener;
+//
+//  @override
+//  void initState() {
+//    super.initState();
+//    listener = () {
+//      setState(() {});
+//    };
+//  }
+//
+//  void createVideo() {
+//    if (playerController == null) {
+//      playerController = VideoPlayerController.asset(
+//        "videos/sample.mp4")
+//        ..addListener(listener)
+//        ..setVolume(1.0)
+//        ..initialize()
+//        ..play();
+//    } else {
+//      if (playerController.value.isPlaying) {
+//        playerController.pause();
+//      } else {
+//        playerController.initialize();
+//        playerController.play();
+//      }
+//    }
+//  }
+//
+//  @override
+//  void deactivate() {
+//    playerController.setVolume(0.0);
+//    playerController.removeListener(listener);
+//    super.deactivate();
+//  }
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      appBar: AppBar(
+//        title: Text('Video Example'),
+//      ),
+//      body: Center(
+//          child: AspectRatio(
+//              aspectRatio: 16 / 9,
+//              child: Container(
+//                child: (playerController != null
+//                    ? VideoPlayer(
+//                  playerController,
+//                )
+//                    : Container()),
+//              ))),
+//      floatingActionButton: FloatingActionButton(
+//        onPressed: () {
+//          createVideo();
+//          playerController.play();
+//        },
+//        child: Icon(Icons.play_arrow),
+//      ),
+//    );
+//  }
+//}
+*/
